eb3d5a9f4e1fd2eded1323e0bdd38bc10d845892
Jadeilton <jadeiltondm@gmail.com>
2024-03-18 10:42:22 -0300

    feat

---
+++ src/test/java/lista/funcionarios/funcionariosLista/FuncionariosListaApplicationTests.java
@@ -0,0 +1,13 @@
+package lista.funcionarios.funcionariosLista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionariosListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
