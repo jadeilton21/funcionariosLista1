d4c2d01c2406a84207d408a9ee311e9b552f535f
Jadeilton <jadeiltondm@gmail.com>
2024-03-18 10:42:18 -0300

    feat

---
+++ src/main/java/lista/funcionarios/funcionariosLista/FuncionariosListaApplication.java
@@ -0,0 +1,13 @@
+package lista.funcionarios.funcionariosLista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionariosListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionariosListaApplication.class, args);
+	}
+
+}
