2007f4753526c47e7e7c09c2e1c92118ae0a399c
Jadeilton <jadeiltondm@gmail.com>
2024-03-19 16:16:41 -0300

    feat: Funcionario add @NoArgsContructor, @AllArgsConstructor

--- src/main/java/lista/funcionarios/funcionariosLista/doMain/Funcionario.java
+++ src/main/java/lista/funcionarios/funcionariosLista/doMain/Funcionario.java
@@ -1,4 +1,11 @@
 package lista.funcionarios.funcionariosLista.doMain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Funcionario {
 }
