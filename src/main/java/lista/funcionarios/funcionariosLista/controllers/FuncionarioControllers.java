2619b0e3516ccccf5744652c0c1c38863cf608b9
Jadeilton <jadeiltondm@gmail.com>
2024-03-19 17:07:12 -0300

    feat: create FuncionarioControllers

--- src/main/java/lista/funcionarios/funcionariosLista/controllers/FuncionarioControllers.java
+++ src/main/java/lista/funcionarios/funcionariosLista/controllers/FuncionarioControllers.java
@@ -1,13 +1,30 @@
 package lista.funcionarios.funcionariosLista.controllers;
 
 
+import lista.funcionarios.funcionariosLista.doMain.Funcionario;
+import lista.funcionarios.funcionariosLista.service.FuncionarioService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/funcionario")
+@RequestMapping("/api/funcionario")
 public class FuncionarioControllers {
+
+
+    private final FuncionarioService funcionarioService;
+
+
+
+    public ResponseEntity<List<Funcionario>> getFuncionario(){
+
+        List<Funcionario> funcionarios =this.funcionarioService.getFuncionarioFromLibriary();
+        return ResponseEntity.ok().body(funcionarios);
+    }
 
 
 }
