1973df2f89d79136d363ae3c7eeec009644ea469
Jadeilton <jadeiltondm@gmail.com>
2024-03-19 13:42:16 -0300

    feat: add enableFeingClients

--- src/main/java/lista/funcionarios/funcionariosLista/FuncionariosListaApplication.java
+++ src/main/java/lista/funcionarios/funcionariosLista/FuncionariosListaApplication.java
@@ -2,8 +2,10 @@ package lista.funcionarios.funcionariosLista;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FuncionariosListaApplication {
 
 	public static void main(String[] args) {
