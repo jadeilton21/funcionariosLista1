9c8e562eee43e4c4d64e3c4e2cf29dd75617b146
Jadeilton <jadeiltondm@gmail.com>
2024-03-19 16:37:01 -0300

    feat: create arguments in funcionario

--- src/main/java/lista/funcionarios/funcionariosLista/doMain/Funcionario.java
+++ src/main/java/lista/funcionarios/funcionariosLista/doMain/Funcionario.java
@@ -8,4 +8,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Funcionario {
+
+
+    private String nome;
+    private String sobreNome;
+    private String email;
+    private String idade;
+    private String funcao;
+    private String cidade;
+
 }
