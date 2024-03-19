78637081f599c3c2cb92d9c5342b484e20d2c736
Jadeilton <jadeiltondm@gmail.com>
2024-03-19 16:38:34 -0300

    feat: create funcionario mapping

--- src/main/java/lista/funcionarios/funcionariosLista/controllers/FuncionarioControllers.java
+++ src/main/java/lista/funcionarios/funcionariosLista/controllers/FuncionarioControllers.java
@@ -1,4 +1,13 @@
 package lista.funcionarios.funcionariosLista.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/funcionario")
 public class FuncionarioControllers {
+
+
 }
