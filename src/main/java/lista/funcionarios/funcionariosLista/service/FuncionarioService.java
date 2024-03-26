e46c9893b3134098e747ae459869174d196d2a0b
Jadeilton <jadeiltondm@gmail.com>
2024-03-25 22:19:47 -0300

    feat: create method

--- src/main/java/lista/funcionarios/funcionariosLista/service/FuncionarioService.java
+++ src/main/java/lista/funcionarios/funcionariosLista/service/FuncionarioService.java
@@ -1,13 +1,14 @@
 package lista.funcionarios.funcionariosLista.service;
 
+import lista.funcionarios.funcionariosLista.doMain.Funcionario;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FuncionarioService {
 
 
-
-
-
-
+    public List<Funcionario> getFuncionarioFromLibriary() {
+    }
 }
