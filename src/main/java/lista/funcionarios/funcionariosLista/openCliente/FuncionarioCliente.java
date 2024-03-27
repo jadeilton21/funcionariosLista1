23cd81fbabd4c11b4a689c9d7df6e9ff1b99084e
Jadeilton <jadeiltondm@gmail.com>
2024-03-27 07:35:02 -0300

    feat: create method in FuncionarioCliente

--- src/main/java/lista/funcionarios/funcionariosLista/openCliente/FuncionarioCliente.java
+++ src/main/java/lista/funcionarios/funcionariosLista/openCliente/FuncionarioCliente.java
@@ -1,10 +1,13 @@
 package lista.funcionarios.funcionariosLista.openCliente;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "",url = "")
 public interface FuncionarioCliente {
+    @GetMapping
+    FuncionarioResponseList getFuncionario(){
 
-
+    }
 
 }
