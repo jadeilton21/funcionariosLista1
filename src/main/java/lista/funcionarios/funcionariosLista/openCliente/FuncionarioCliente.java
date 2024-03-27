85b43e11bba6533861d0b2ec5af85399bd34ee39
Jadeilton <jadeiltondm@gmail.com>
2024-03-27 07:34:01 -0300

    feat: create interface FuncionarioCliente

--- src/main/java/lista/funcionarios/funcionariosLista/openCliente/FuncionarioCliente.java
+++ src/main/java/lista/funcionarios/funcionariosLista/openCliente/FuncionarioCliente.java
@@ -1,4 +1,10 @@
 package lista.funcionarios.funcionariosLista.openCliente;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "",url = "")
 public interface FuncionarioCliente {
+
+
+
 }
