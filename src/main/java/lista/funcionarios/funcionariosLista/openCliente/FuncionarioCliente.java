066d0dac4a3ef6e18aa44d0d70e79156cdebef38
Jadeilton <jadeiltondm@gmail.com>
2024-03-27 07:43:55 -0300

    feat: FuncionarioResponseList created

--- src/main/java/lista/funcionarios/funcionariosLista/openCliente/FuncionarioCliente.java
+++ src/main/java/lista/funcionarios/funcionariosLista/openCliente/FuncionarioCliente.java
@@ -1,13 +1,12 @@
 package lista.funcionarios.funcionariosLista.openCliente;
 
+import lista.funcionarios.funcionariosLista.response.FuncionarioResponseList;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "",url = "")
 public interface FuncionarioCliente {
     @GetMapping
-    FuncionarioResponseList getFuncionario(){
-
-    }
+    FuncionarioResponseList getFuncionario();
 
 }
