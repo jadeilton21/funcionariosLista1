ef189400ba8decfb6890c889514bab35eb2300f0
Jadeilton <jadeiltondm@gmail.com>
2024-03-28 18:29:57 -0300

    feat: FuncionarioResponseList created

--- src/main/java/lista/funcionarios/funcionariosLista/response/FuncionarioResponseList.java
+++ src/main/java/lista/funcionarios/funcionariosLista/response/FuncionarioResponseList.java
@@ -1,4 +1,19 @@
 package lista.funcionarios.funcionariosLista.response;
 
+import lista.funcionarios.funcionariosLista.doMain.Funcionario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class FuncionarioResponseList {
+
+    private List<Funcionario> funcionarioList;
+
+
+
 }
